40f67566ec171ee5b2df436b1e68d35e9b5241d1
mirian.khajishvili@gmail.com <mirian.khajishvili@gmail.com>
2023-04-01 17:33:07 +0400

    1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080/'
+  apiUrl: 'web-production-dcf6.up.railway.app'
   // apiUrl: 'https://seal-app-l3d7u.ondigitalocean.app/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/'
+  apiUrl: 'web-production-dcf6.up.railway.app'
 };
 
 /*
